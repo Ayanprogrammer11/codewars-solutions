6f5e5640369b3ad5d4b9fb0402b527e5fc245ab1
Ayan Liaqat <108534604+Ayanprogrammer11@users.noreply.github.com>
2024-06-06 19:15:34 +0500

    Update 7kyu_summing-number-digits.js
    Added 2 alternate solutions that is more efficient than the existing ones.

--- js/7kyu_summing-number-digits.js
+++ js/7kyu_summing-number-digits.js
@@ -7,3 +7,20 @@
 */
 
 const sumDigits = number => Math.abs(number).toString().split('').map(Number).reduce((a,b) => a + b);
+
+// 1. This approach eliminates the use of map which COULD be expensive for larger arrays. It uses javascript type coercion power to automatically handle that for us
+const sumDigits1 = number => Math.abs(number).toString().split('').reduce((a, b) => a + +b, 0);
+
+// 2. A little bit longer solution but far more efficient.
+const sumDigits2 = num => {
+  const absNum = Math.abs(num);
+  let sum = 0;
+
+  while (absNum > 0) {
+    sum += absNum % 10;
+    absNum = Math.floor(absNum / 10);
+  }
+
+  return sum;
+};
+// It reduces unnecessary array creation using split, and also eliminates the use of map. 
